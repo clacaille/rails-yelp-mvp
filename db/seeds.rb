591156e7e6b0cf80035cc4930b65cb008212d56a
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-08 11:51:28 +0100

    restaurants controller + action index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "French",
+    phone_number: "0102030405"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "French",
+    phone_number: "0107080910"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "Spanish",
+    phone_number: "0600102030"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
