6db7e185cfd1462c07b6dff06f420d9aba83a3e5
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-08 15:27:49 +0100

    added review field in restaurants#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
+  root "restaurants#index"
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [ :create ]
   end
 end
