d7075e14e6008b735a0d532a3f8e1961ffcfaf3a
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-08 12:47:01 +0100

    models really done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
   validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
